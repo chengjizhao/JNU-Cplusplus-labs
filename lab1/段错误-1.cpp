7b880c7b978dd890dcb9718caa8a8f02b753793a
2021102169 <143705635+2021102169@users.noreply.github.com>
2023-09-03 22:08:54 +0800

    Add files via upload

---
+++ lab1/段错误-1.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    printf("%s\n",a); //把整数当成了字符串
+    return 0;
+}
---
+++ lab1/段错误-2.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",a);   //&a写成了a
+    printf("%d\n",a);
+    return 0;
+}
