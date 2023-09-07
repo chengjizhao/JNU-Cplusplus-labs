452057449d702fd2f41a3a1684f18782493f2f5c
2021100452 <143698206+2021100452@users.noreply.github.com>
2023-09-07 22:21:44 +0800

    Add files via upload

---
+++ lab1/2021100452/a.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main() {
+	int b = 10;
+	printf("%s\n", b);
+	return 0;
+}
+//把整数当做字符串打印 整数被当做地址 而证书代表的地址不存在或者不可访问
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main(){
+//     char * c = "hello world";
+//     c[1] = 'H';
+// }
