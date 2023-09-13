7918f67307bd014e3306a45e0647e37055477034
2021102153 <143695219+2021102153@users.noreply.github.com>
2023-09-13 10:48:43 +0800

    Add files via upload

---
+++ lab2/2021102153/student.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fstream>
+#include <iostream>
+#include <cstdio> 
+#include <vector>
+#include <streambuf>
+#include <sstream>
+using namespace std;
+
+void query();//查询函数
+void enter();//录入成绩
+void nalgrad();
