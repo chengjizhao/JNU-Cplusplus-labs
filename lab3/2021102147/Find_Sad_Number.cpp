551c20608423a8a28fb6cff5ba471830707fcf96
LouisZ <88891403+LouisAE@users.noreply.github.com>
2023-09-14 21:47:26 +0800

    Add comment

--- lab3/2021102147/Find_Sad_Number.cpp
+++ lab3/2021102147/Find_Sad_Number.cpp
@@ -7,20 +7,21 @@ bool isSad(int n)
 {
 	vector<int>already;
 	vector<int>::iterator it;
-	while (n != 1)
+	while (n != 1)// 当最后的结果为1时，是悲伤数
 	{
 		int sum = 0;
 		while (n > 0)
 		{
-			sum += (n % 10) * (n % 10);
-			n /= 10;
+			sum += (n % 10) * (n % 10);// 通过取余运算取末位并平方、累加
+			n /= 10; //去掉末位
 		}
+		// 找一下当前的和(sum)有没有出现过（防止死循环）
 		it = std::find(already.begin(), already.end(), sum);
-		if ( it != already.end())
+		if (it != already.end())
 		{
 			return false;
 		}
-		n = sum;
+		n = sum;// 把sum作为下一次运算的数字
 		already.push_back(sum);
 	}
 	return true;
