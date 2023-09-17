e59bb0303a3b0cae7583a2ad0a1e53344dd7544d
LouisZ <88891403+LouisAE@users.noreply.github.com>
2023-09-17 09:55:57 +0800

    Update code to reduce difference between two calculations

--- lab3/2021102147/Vector_and_List_compare.cpp
+++ lab3/2021102147/Vector_and_List_compare.cpp
@@ -27,27 +27,32 @@ int main()
 	clock_t start = clock();
 	clock_t vector_time = 0;
 	clock_t list_time = 0;
-	// 在vector的随机位置插入1000个元素并比较
+	
+	// 在vector的随机位置插入n个元素并比较
 	// 取模是为了限制范围，防止越界
+	auto iter_vec = vec.begin();
 	for (int i = 0; i < testnum; i++)
 	{
-		vec.insert((vec.begin() + (rand() % (size - 1))), 0);
+		advance(iter_vec, rand() % (size - 1));//随机移动迭代器
+		vec.insert(iter_vec, 0);
 		++size;
+		iter_vec = vec.begin();//迭代器回到头部
 	}
 	vector_time = clock() - start;
 
 	// list有所不同，它存储的数据不连续，不能使用加法来定位
 	start = clock();
 	size = 50;
-	auto iter = li.begin();
+	auto iter_li = li.begin();
 	for (int i = 0; i < testnum; i++)
 	{
-		advance(iter, rand() % (size - 1));//随机移动迭代器
-		li.insert(iter, 0);
+		advance(iter_li, rand() % (size - 1));
+		li.insert(iter_li, 0);
 		++size;
-		iter = li.begin();//迭代器回到头部
+		iter_li = li.begin();
 	}
 	list_time = clock() - start;
+	
 	cout << "Vector time: " << vector_time << "ms" << endl;
 	cout << "List time: " << list_time << "ms" << endl;
 }
