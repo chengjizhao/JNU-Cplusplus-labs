aff5ddc52dd195dbebc832d8f027ff111a80297b
2020101283 <143699951+2020101283@users.noreply.github.com>
2023-09-20 20:27:29 +0800

    Update vector_vs_list.cpp

--- lab3/2020101283/vector_vs_list.cpp
+++ lab3/2020101283/vector_vs_list.cpp
@@ -14,7 +14,8 @@ double begin_time, end_time;
 int main()
 {
 	cout << "当前初始容量：" << Begin_num << "，" << "插入数量：" << Insert_num << endl;
-	//vector
+	
+	//计算vector插入用时
 	vector<int> my_vector(Begin_num);
 	begin_time = clock();//获取开始时间 
 	for (int j = 0; j < Insert_num; j++) 
@@ -24,7 +25,7 @@ int main()
 	cout << (double)((end_time - begin_time) / 1000) << endl;
 	
 	
-	//list
+	//计算list插入用时
 	list<int> my_list(Begin_num);
 	list<int>::iterator it;
 	it = my_list.begin();
