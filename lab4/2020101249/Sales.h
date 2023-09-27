151a342a2f9fbe310b792786aeee96929994074d
李卓鸿 <143866890+umhffhch@users.noreply.github.com>
2023-09-27 20:55:56 +0800

    Add files via upload

---
+++ lab4/2020101249/Sales.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace SALES
+{
+	const int QUARTERS = 4;
+	struct Sales {
+		double sales[QUARTERS];
+		double max,min,average;
+	};
+	void setSales(Sales &s);
+	void setSales(Sales &s, const double ar[], int n);
+	void showSales(const Sales& s);
+
+}
