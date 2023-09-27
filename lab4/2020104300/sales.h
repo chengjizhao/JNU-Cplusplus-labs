21deca1eb41fb1ac0300edb1a302cc5b93d35858
Cai Yuqing <114802169+233CeciliaCC@users.noreply.github.com>
2023-09-27 22:41:53 +0800

    Create sales.h

---
+++ lab4/2020104300/sales.h
@@ -0,0 +1,15 @@
+// sales.h
+
+namespace SALES {
+    const int QUARTERS = 4;
+    struct Sales {
+        double sales[QUARTERS];
+        double max;
+        double min;
+        double average;
+    };
+
+    void setSales(Sales &s, const double ar[], int n);
+    void setSales(Sales &s);
+    void showSales(const Sales& s);
+}
