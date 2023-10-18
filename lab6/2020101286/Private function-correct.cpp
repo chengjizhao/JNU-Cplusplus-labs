a1c42ff22a0c34bb4522f9472ab3ec1d9ad33da1
jiangsongfei <143695750+jiangsongfei@users.noreply.github.com>
2023-10-18 21:21:43 +0800

    Add files via upload

---
+++ lab6/2020101286/Private function-correct.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+class MyClass 
+{
+private:
+    void privateFunc() {
+        std::cout << "Private function called" << std::endl;
+    }
+
+public:
+    void publicFunc() {
+        std::cout << "Public function called" << std::endl;
+        privateFunc(); // 在类的内部可以直接调用私有成员函数
+    }
+};
+
+int main() {
+    MyClass obj;
+    obj.publicFunc(); // 在类的外部可以调用公有成员函数
+
+    // obj.privateFunc();  // 错误！无法在类的外部直接调用私有成员函数
+
+    return 0;
+}
