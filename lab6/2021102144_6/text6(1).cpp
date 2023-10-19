043751bdbab1ca1369da1f5f6dd23187e55f6926
233jekey <130957338+233jekey@users.noreply.github.com>
2023-10-19 12:03:22 +0800

    Add files via upload

---
+++ lab6/2021102144_6/text6(1).cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class Myclass
+{
+    private:
+    int a=10;
+    public:
+    void f();
+};
+void Myclass::f()
+{
+    cout<<a<<endl;
+}
+void F(void(Myclass::*f)(),Myclass &M)
+{
+    (M.*f)();
+}
+int main()
+{
+    Myclass M;
+    F(&Myclass::f,M);
+    return 0;
+}
