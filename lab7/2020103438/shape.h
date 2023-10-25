76419c5e249ee7a640b164135a2e7474130c55a1
2020103438 <143695733+2020103438@users.noreply.github.com>
2023-10-26 00:53:15 +0800

    Add files via upload

---
+++ lab7/2020103438/shape.h
@@ -0,0 +1,37 @@
+#pragma once
+#ifndef Shape_h
+#define Shape_h
+#define PI 3.141;
+#include <iostream>
+using namespace std;
+class Shape
+{
+public:
+    double getarea();
+    void getperimeter();
+};
+class Circle : public Shape
+{
+public:
+    Circle();
+    double getarea();
+    void getperimeter();
+    void set(int a);
+
+private:
+    int r;
+};
+
+class Rectangle : public Shape
+{
+public:
+    Rectangle();
+    double getarea();
+    void getperimeter();
+    void set(int a, int b);
+
+private:
+    int length;
+    int width;
+};
+#endif
