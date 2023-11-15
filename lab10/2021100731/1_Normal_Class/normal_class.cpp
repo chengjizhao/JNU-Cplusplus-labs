9c83a576226f3e6096df52756a4eec0b07c3b7ac
EzorienQaQ <139998251+EzorienQaQ@users.noreply.github.com>
2023-11-15 10:45:59 +0800

    Add files via upload

---
+++ lab10/2021100731/1_Normal_Class/normal_class.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+class Normal_Class
+{
+public:
+    int param;
+    void testFunc()
+    {
+        cout << "testFunc is called!" << endl;
+    }
+};
+
+typedef void (Normal_Class::*Funcpointer)(void);
+
+int main()
+{   
+    Normal_Class dummy1;
+
+    int* pClass = reinterpret_cast<int*>(&dummy1);/*get ClassAddr*/
+    int* pParam= &dummy1.param;/*get ParamAddr*/
+
+    Funcpointer pFunc = &Normal_Class::testFunc;/*get FuncAddr*/
+
+    cout << "This is ClassAddr: " << pClass << endl;
+    cout << "This is ParamAddr: " << pParam << endl;
+    cout << "This is FuncAddr : " << pFunc << endl;
+    (dummy1.*pFunc)();/*Use FuncAddr to call this func*/
+
+    return 0;
+}
