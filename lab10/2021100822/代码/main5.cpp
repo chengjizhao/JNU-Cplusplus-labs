da33e8b476ca457571d4b886931891f9cdcf929c
2021100822 <143699159+Guangyuzhang123@users.noreply.github.com>
2023-11-23 10:49:03 +0800

    Update main5.cpp

--- lab10/2021100822/代码/main5.cpp
+++ lab10/2021100822/代码/main5.cpp
@@ -2,17 +2,17 @@
 ##多继承的类对象包含多个基类的成员变量，并且可能有虚函数表指针。
 
 class Base1 {
-公共:
+public:
     int data1;
 };
 
 class Base2 {
-公共:
+public:
     int data2;
 };
 
 class Derived : 公共 Base1, 公共 Base2 {
-公共:
+public:
     int derivedData;
 };
 
