ad415af1ad31c164345eab26b64c510c7fb6af50
EzorienQaQ <139998251+EzorienQaQ@users.noreply.github.com>
2023-12-07 10:02:33 +0800

    Add files via upload

---
+++ lab12/2021100731/build/CMakeFiles/lib_create.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib_create.dir/src/add/add.cpp.obj"
+  "CMakeFiles/lib_create.dir/src/add/add.cpp.obj.d"
+  "CMakeFiles/lib_create.dir/src/main.cpp.obj"
+  "CMakeFiles/lib_create.dir/src/main.cpp.obj.d"
+  "lib_create.exe"
+  "lib_create.exe.manifest"
+  "lib_create.pdb"
+  "liblib_create.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lib_create.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab12/2021100731/build/CMakeFiles/main.dir/src/add/add.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/main.dir/src/add/add.cpp.obj: \
+ D:\C++Files\Homework\work12\lib_create\src\add\add.cpp \
+ D:/C++Files/Homework/work12/lib_create/include/add/add.h
