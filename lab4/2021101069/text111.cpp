c700e8631f6c135939db7d50e3b3da59538cee44
2021101069 <143694706+2021101069@users.noreply.github.com>
2023-12-08 13:36:43 +0800

    Update text111.cpp
    上传错误，修改后二次上传

--- lab4/2021101069/text111.cpp
+++ lab4/2021101069/text111.cpp
@@ -1,27 +1,13 @@
 #include "Sales.h"  
 #include <iostream>  
-#include <algorithm>  
   
-namespace SALES   
-{   
-void Sales::setSales(const double ar[], int n) {  
-    if (n > QUARTERS) {  
-        std::cout << "Error: trying to set more than " << QUARTERS << " sales numbers." << std::endl;  
-        return;  
-    }  
-    for (int i = 0; i < n; ++i) {  
-        sales[i] = ar[i];  
-    }  
-    std::sort(sales, sales + n);  
-    max = sales[n-1];  
-    min = sales[0];  
-}  
-  
-void Sales::showSales() {  
-    std::cout << "Sales: ";  
-    for (int i = 0; i < QUARTERS; ++i) {  
-        std::cout << sales[i] << " ";  
-    }  
-    std::cout << "\nMax: " << max << "\nMin: " << min << std::endl;  
-}  
-} // namespace SALES
+int main() {  
+    SALES::Sales s1, s2;  
+    double sales1[] = {1000, 2000, 3000, 4000};  
+    double sales2[] = {500, 1500, 2500, 3500};  
+    s1.setSales(sales1, 4);  
+    s2.setSales(sales2, 4);  
+    s1.showSales();  
+    s2.showSales();  
+    return 0;  
+}
