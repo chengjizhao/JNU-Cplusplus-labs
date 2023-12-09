09aa4ca86b8a3362fc480bfb4e96f3bc6d8cb86e
jnunoob <143694965+jnunoob@users.noreply.github.com>
2023-12-09 23:29:16 +0800

    Add files via upload

---
+++ lab12/2020100679/demo_17/main.cpp
@@ -0,0 +1,9 @@
+#include "math_functions.h"
+#include <iostream>
+
+int main() {
+    int a = 10, b = 5;
+    std::cout << "Sum: " << add(a, b) << std::endl;
+    std::cout << "Difference: " << subtract(a, b) << std::endl;
+    return 0;
+}
---
+++ lab12/2020100679/demo_17/math_functions.h
@@ -0,0 +1,7 @@
+#ifndef MATH_FUNCTIONS_H
+#define MATH_FUNCTIONS_H
+
+int add(int a, int b);
+int subtract(int a, int b);
+
+#endif // MATH_FUNCTIONS_H
