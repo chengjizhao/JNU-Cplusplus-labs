280169ebb14fc7ebd8ea376bfbde00ff8eedeacb
2021100822 <143699159+Guangyuzhang123@users.noreply.github.com>
2023-12-11 19:58:09 +0800

    Add files via upload

---
+++ lab12/2021100822/code/build/CMakeFiles/MyExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyExecutable.dir/src/main.cpp.obj"
+  "CMakeFiles/MyExecutable.dir/src/main.cpp.obj.d"
+  "bin/MyExecutable.exe"
+  "bin/MyExecutable.exe.manifest"
+  "bin/MyExecutable.pdb"
+  "libMyExecutable.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
