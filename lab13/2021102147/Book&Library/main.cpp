00823485c3ece49cb959fdd27e1685daf85aa7cb
LouisZ <88891403+LouisAE@users.noreply.github.com>
2023-12-11 15:52:02 +0800

    Submit Library source code

---
+++ lab13/2021102147/Book&Library/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+#include "Library.h"
+
+int main()
+{
+	Library lib;
+	lib.addBook("Ero manga sensei", "Fushimi Tsukasa");
+	lib.addBook("Wan shui qian shan zou bian", "Sanmao");
+	lib.addBook("Das Kapital", "Karl Marx");
+
+	lib.printAllBooks();
+
+	lib.removeBook("Das Kapital");
+	lib.printAllBooks();
+}
