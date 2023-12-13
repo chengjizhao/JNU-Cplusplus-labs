225d991f47588f6cabbee2b05050a9d5b1f8998a
Holdenfeng <143694517+Holdenfeng@users.noreply.github.com>
2023-12-13 21:00:11 +0800

    Add files via upload

---
+++ lab9/2020104355/vehicle.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class Vehicle {
+public:
+    int speed;
+
+    virtual void start() {
+        cout << "Vehicle started" << endl;
+    }
+};
+class Car : virtual public Vehicle {
+public:
+    void start() override {
+        cout << "Car started" << endl;
+    }
+};
+class Boat : virtual public Vehicle {
+public:
+    void start() override {
+        cout << "Boat started" << endl;
+    }
+};
+class AmphibiousVehicle : public Car, public Boat {
+public:
+    void start() override {
+        Car::start();
+        Boat::start();
+    }
+};
+int main() {
+    AmphibiousVehicle amphibiousVehicle;
+    amphibiousVehicle.start();
+    return 0;
+}
