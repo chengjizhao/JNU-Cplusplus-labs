3f86a8ead7839ec2e87548c3e3bec846a03e6aaa
Huangjr03 <143695788+Huangjr03@users.noreply.github.com>
2023-12-25 10:41:33 +0800

    Add files via upload

---
+++ lab14/2021102167/bank.cpp
@@ -0,0 +1,59 @@
+#include <iostream>  
+#include <thread>  
+#include <mutex>  
+#include <random>  
+#include <chrono>  
+
+class BankAccount {
+public:
+    BankAccount(int balance) : balance(balance) {}
+
+    void deposit(int amount) {
+        std::unique_lock<std::mutex> lock(mutex_);
+        balance += amount;
+        std::cout << "Deposited " << amount << ", balance is now " << balance << std::endl;
+    }
+
+    void withdraw(int amount) {
+        std::unique_lock<std::mutex> lock(mutex_);
+        if (balance >= amount) {
+            balance -= amount;
+            std::cout << "Withdrew " << amount << ", balance is now " << balance << std::endl;
+        }
+        else {
+            std::cout << "Insufficient funds!" << std::endl;
+        }
+    }
+
+private:
+    int balance;
+    std::mutex mutex_;
+};
+
+void depositThread(BankAccount& account, std::default_random_engine& generator) {
+    while (true) {
+        std::uniform_int_distribution<int> distribution(1, 100);
+        int amount = distribution(generator);
+        account.deposit(amount);
+        std::this_thread::sleep_for(std::chrono::milliseconds(generator() % 1000));
+    }
+}
+
+void withdrawThread(BankAccount& account, std::default_random_engine& generator) {
+    while (true) {
+        std::uniform_int_distribution<int> distribution(1, 100);
+        int amount = distribution(generator);
+        account.withdraw(amount);
+        std::this_thread::sleep_for(std::chrono::milliseconds(generator() % 1000));
+    }
+}
+
+int main() {
+    std::default_random_engine generator;
+    BankAccount account(1000);
+    std::thread depositThread([&account, &generator]() { depositThread(account, generator); });
+    std::thread withdrawThread([&account, &generator]() { withdrawThread(account, generator); });
+    depositThread.join();
+    withdrawThread.join(); 
+    return 0;
+}
