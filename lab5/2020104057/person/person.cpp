a87c86c9c7da9047943c67dc8c99b30a620b63f5
1wangruoxi <144901076+1wangruoxi@users.noreply.github.com>
2023-12-30 20:13:53 +0800

    Create person.cpp

---
+++ lab5/2020104057/person/person.cpp
@@ -0,0 +1,24 @@
+#include "person.h"
+using namespace std;
+
+// 构造函数储存信息
+Person::Person() : name(""), id(""), birthday(""), gender('M'), height(0), weight(0) {}
+Person::Person(const string &n) : name(n), id(""), birthday(""), gender('M'), height(0), weight(0) {}
+Person::Person(const string &n, const string &i) : name(n), id(i), birthday(""), gender('M'), height(0), weight(0) {}
+Person::Person(const string &n, const string &i, const string &b, char g, double h, double w) 
+    : name(n), id(i), birthday(b), gender(g), height(h), weight(w) {}
+
+void Person::setName(const string &n) { name = n; }
+void Person::setID(const string &i) { id = i; }
+void Person::setBirthday(const string &b) { birthday = b; }
+void Person::setGender(char g) { gender = g; }
+void Person::setHeight(double h) { height = h; }
+void Person::setWeight(double w) { weight = w; }
+
+// 查询需要的信息
+string Person::getName() const { return name; }
+string Person::getID() const { return id; }
+string Person::getBirthday() const { return birthday; }
+char Person::getGender() const { return gender; }
+double Person::getHeight() const { return height; }
+double Person::getWeight() const { return weight; }
