93af161136f117303e39b3271b4df20db130e91a
2021102150 <143695281+2021102150@users.noreply.github.com>
2023-12-31 23:14:46 +0800

    Add files via upload

---
+++ lab13/2021102150a.cpp/person/Person.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+class Address;
+
+class Person {
+ public:
+  Person(const std::string& name, int age, Address& addr);
+  std::string getName() const;
+  int getAge() const;
+  void printAddr() const;
+
+ private:
+  std::string name;
+  int age;
+  std::shared_ptr<Address> addr;
+};
+
+class Address {
+ public:
+  Address(const char* addr);
+  void printAddr() const;
+  void setAddr(const char* addr);
+  void addPerson(std::shared_ptr<Person> sp);
+  const std::vector<std::weak_ptr<Person>>& getPersons() const;
+
+ private:
+  std::string addr;
+  std::vector<std::weak_ptr<Person>> persons;
+};
