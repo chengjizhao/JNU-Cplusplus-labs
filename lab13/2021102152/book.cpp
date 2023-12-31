f382fd377b0bda9e1a05c2cc5f04a744f663dbc7
2021102152 <143695216+2021102152@users.noreply.github.com>
2023-12-31 23:28:45 +0800

    Add files via upload

---
+++ lab13/2021102152/book.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+class Book
+{
+public:
+    Book(const std::string &title, const std::string &author)
+        : title(title), author(author) {}
+
+    std::string getTitle() const { return title; }
+    std::string getAuthor() const { return author; }
+
+private:
+    std::string title;
+    std::string author;
+};
+
+class Library
+{
+public:
+    void addBook(const std::shared_ptr<Book> &book)
+    {
+        books.push_back(book);
+    }
+
+    void removeBook(const std::shared_ptr<Book> &book)
+    {
+        for (auto it = books.begin(); it != books.end(); ++it)
+        {
+            if (*it == book)
+            {
+                books.erase(it);
+                break;
+            }
+        }
+    }
+
+    void displayBooks() const
+    {
+        for (const auto &book : books)
+        {
+            std::cout << "Title: " << book->getTitle() << ", Author: " << book->getAuthor() << std::endl;
+        }
+    }
+
+private:
+    std::vector<std::shared_ptr<Book>> books;
+};
+
+int main()
+{
+    Library library;
+    std::shared_ptr<Book> book1 = std::make_shared<Book>("Harry Potter and the Sorcerer's Stone", "J.K. Rowling");
+    std::shared_ptr<Book> book2 = std::make_shared<Book>("To Kill a Mockingbird", "Harper Lee");
+    library.addBook(book1);
+    library.addBook(book2);
+    library.displayBooks();   
+    library.removeBook(book1); 
+    library.displayBooks();    
+    return 0;
+}
