3608719bd3fcf51031d45944c2dd808e5a634000
Frank <89758950+Frank2045@users.noreply.github.com>
2024-01-02 23:43:06 +0800

    Add files via upload

---
+++ lab12/2020100097/include/mylib.h
@@ -0,0 +1,6 @@
+#ifndef MYLIB_H  
+#define MYLIB_H  
+
+int kickkick(int a,int b);  
+  
+#endif
---
+++ lab12/2020100097/main.cpp
@@ -0,0 +1,8 @@
+#include "mylib.h"  
+#include <iostream>  
+  
+int main() {  
+    int result = kickkick(3, 6);
+    std::cout << "Result: " << result << std::endl;
+    return 0; 
+}
