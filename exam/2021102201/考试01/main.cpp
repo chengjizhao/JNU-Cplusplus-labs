ff108d47b0e7ed4751af391b266493e82341831a
MnsxY <101882313+mangNSXying@users.noreply.github.com>
2024-01-04 15:18:34 +0800

    Add files via upload

---
+++ exam/2021102201/考试01/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Vehicle
+{
+public:
+    virtual void start() { cout << "Vehicle starting..." << endl; }
+};
+
+class Car : public Vehicle
+{
+public:
+    void start() override { cout << "Car starting..." << endl; }
+};
+
+class Bike : public Vehicle
+{
+public:
+    void start() override { cout << "Bike starting..." << endl; }
+};
+
+int main()
+{
+    Vehicle* vehicles[2];
+    Car car;
+    Bike bike;
+
+    vehicles[0] = &car;
+    vehicles[1] = &bike;
+
+    for (int i = 0; i < 2; i++) {
+        vehicles[i]->start();
+    }
+
+    return 0;
+}
