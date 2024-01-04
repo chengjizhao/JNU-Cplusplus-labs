b05f85e48d9fbcf6b2ceb8e18437f821c66285ce
Lidadapan <149048041+Lidadapan@users.noreply.github.com>
2024-01-04 11:00:47 +0800

    Add files via upload

---
+++ lab9/2020100628/multiHeritage.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+class Device
+{
+protected:
+    std::string brand;
+
+public:
+    Device(const std::string& b) : brand(b) {}
+    void powerOn() { std::cout << "Powering on " << brand << "device" << std::endl; }
+};
+
+class Computer : virtual public Device
+{
+public:
+    Computer(const std::string& b) : Device(b) {}
+    void boot() { std::cout << "Booting " << brand << " computer" << std::endl; }
+    virtual void runApp() { std::cout << "Running application on " << brand << " computer" << std::endl; }
+};
+
+class Phone : virtual public Device
+{
+public:
+    Phone(const std::string& b) : Device(b) {}
+    void call() { std::cout << "Making a phone call with " << brand << " phone" << std::endl; }
+    virtual void runApp() { std::cout << "Running application on " << brand << " phone" << std::endl; }
+};
+
+class SmartDevice : public Computer, public Phone
+{
+public:
+    SmartDevice(const std::string& b) : Device(b), Computer(b), Phone(b) {}
+    void runApp() { std::cout << "Running application on smart device" << std::endl; }
+};
+
+int main() {
+    SmartDevice test("SmartDevice");
+    test.powerOn();
+    test.boot();
+    test.runApp();
+    return 0;
+}
