33844c91d08492942c43d2affb6bc6d3b37b5f23
1wangruoxi <144901076+1wangruoxi@users.noreply.github.com>
2024-01-04 15:16:20 +0800

    Add files via upload

---
+++ exam/2020104057/test_1code.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+// 基类 Animal
+class Animal {
+public:
+  virtual void speak() = 0;
+};
+
+// 派生类 Dog
+class Dog : public Animal {
+public:
+  void speak() override {
+    cout << "Woof!" << endl;
+  }
+};
+
+// 派生类 Cat
+class Cat : public Animal {
+public:
+  void speak() override {
+    cout << "Meow!" << endl;
+  }
+};
+
+int main() {
+  Animal *animals[2];
+  animals[0] = new Dog();
+  animals[1] = new Cat();
+
+  for (int i = 0; i < 2; i++) {
+    animals[i]->speak();
+  }
+
+  return 0;
+}
+
